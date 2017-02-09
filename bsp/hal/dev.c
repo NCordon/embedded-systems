140a84957ef837acbcc045636b21e6ee0b76d454
NCordon <nacho.cordon.castillo@gmail.com>
2017-02-09 19:35:49 +0100

    Llamadas al sistema de Newlib implementadas

--- bsp/hal/dev.c
+++ bsp/hal/dev.c
@@ -7,7 +7,6 @@
 #include <errno.h>
 #include <string.h>
 #include <stdint.h>
-
 #include "system.h"
 
 /*****************************************************************************/
@@ -16,21 +15,20 @@
  * Lista de dispositivos registrados.
  * El máximo número de dispositivos se define en "system.h"
  */
-static bsp_dev_t bsp_dev_list[BSP_MAX_DEV] =
-{
-		/* La primera entrada de la tabla es /dev/null */
-		{
-				"/dev/null",	/* Nombre del dispositivo */
-				0,				/* Identificador */
-				NULL,			/* Función open por defecto */
-				NULL,			/* Función close por defecto */
-				NULL,			/* Función read por defecto */
-				NULL,			/* Función write por defecto */
-				NULL,			/* Función lseek por defecto */
-				NULL,			/* Función fstat por defecto */
-				NULL			/* Función isatty por defecto */
-		}
-		/* El resto del array se inicializa a cero */
+static bsp_dev_t bsp_dev_list[BSP_MAX_DEV] = {
+  /* La primera entrada de la tabla es /dev/null */
+  {
+    "/dev/null",	        /* Nombre del dispositivo */
+    0,			/* Identificador */
+    NULL,			/* Función open por defecto */
+    NULL,			/* Función close por defecto */
+    NULL,			/* Función read por defecto */
+    NULL,			/* Función write por defecto */
+    NULL,			/* Función lseek por defecto */
+    NULL,			/* Función fstat por defecto */
+    NULL			/* Función isatty por defecto */
+  }
+    /* El resto del array se inicializa a cero */
 };
 
 
@@ -49,12 +47,11 @@ static uint32_t bsp_next_dev = 1;
  * El máximo número de descriptores abiertos simultáneamente se define en
  * "system.h"
  */
-static bsp_fd_t bsp_fd_list[BSP_MAX_FD] =
-{
-		{ bsp_dev_list, 0 },    /* Entrada estándar (STDIN) -> /dev/null  */
-		{ bsp_dev_list, 0 },    /* Salida estándar  (STDOUT) -> /dev/null */
-		{ bsp_dev_list, 0 }     /* Error estándar   (STDERR) -> /dev/null */
-		/* El resto de entradas se inicializan a cero */
+static bsp_fd_t bsp_fd_list[BSP_MAX_FD] = {
+  { bsp_dev_list, 0 },    /* Entrada estándar (STDIN) -> /dev/null  */
+  { bsp_dev_list, 0 },    /* Salida estándar  (STDOUT) -> /dev/null */
+  { bsp_dev_list, 0 }     /* Error estándar   (STDERR) -> /dev/null */
+  /* El resto de entradas se inicializan a cero */
 };
 
 /*****************************************************************************/
@@ -73,33 +70,31 @@ static bsp_fd_t bsp_fd_list[BSP_MAX_FD] =
  * @return 			El numero de dispositivo asignado o -1 en caso de error
  */
 int32_t bsp_register_dev (const char  *name,
-		uint32_t id,
-		int (*open)(uint32_t id, int flags, mode_t mode),
-		int (*close)(uint32_t id),
-		ssize_t (*read)(uint32_t id, char *buf, size_t count),
-		ssize_t (*write)(uint32_t id, char *buf, size_t count),
-		off_t (*lseek)(uint32_t id, off_t offset, int whence),
-		int (*fstat)(uint32_t id, struct stat *buf),
-		int (*isatty)(uint32_t id))
-{
-	int32_t index = -1;
-	if (bsp_next_dev < BSP_MAX_DEV)
-	{
-		index = bsp_next_dev;
-		bsp_next_dev++;
+                          uint32_t id,
+                          int (*open)(uint32_t id, int flags, mode_t mode),
+                          int (*close)(uint32_t id),
+                          ssize_t (*read)(uint32_t id, char *buf, size_t count),
+                          ssize_t (*write)(uint32_t id, char *buf, size_t count),
+                          off_t (*lseek)(uint32_t id, off_t offset, int whence),
+                          int (*fstat)(uint32_t id, struct stat *buf),
+                          int (*isatty)(uint32_t id)){
+  int32_t index = -1;
+  if (bsp_next_dev < BSP_MAX_DEV){
+    index = bsp_next_dev;
+    bsp_next_dev++;
+    
+    bsp_dev_list[index].name = name;
+    bsp_dev_list[index].id = id;
+    bsp_dev_list[index].open = open;
+    bsp_dev_list[index].close = close;
+    bsp_dev_list[index].read = read;
+    bsp_dev_list[index].write = write;
+    bsp_dev_list[index].lseek = lseek;
+    bsp_dev_list[index].fstat = fstat;
+    bsp_dev_list[index].isatty = isatty;
+  }
 
-		bsp_dev_list[index].name = name;
-		bsp_dev_list[index].id = id;
-		bsp_dev_list[index].open = open;
-		bsp_dev_list[index].close = close;
-		bsp_dev_list[index].read = read;
-		bsp_dev_list[index].write = write;
-		bsp_dev_list[index].lseek = lseek;
-		bsp_dev_list[index].fstat = fstat;
-		bsp_dev_list[index].isatty = isatty;
-	}
-
-	return index;
+  return index;
 }
 
 /*****************************************************************************/
@@ -108,17 +103,16 @@ int32_t bsp_register_dev (const char  *name,
  * Busca un dispositivo en el sistema
  * @param pathname   Nombre del dispositivo
  */
-bsp_dev_t * find_dev (const char *pathname)
-{
-	uint32_t i;
-	uint32_t len = strlen(pathname) + 1;
+bsp_dev_t * find_dev (const char *pathname){
+  uint32_t i;
+  uint32_t len = strlen(pathname) + 1;
 
-	/* Usamos memcmp() en vez de strcmp() para reducir el tamaño del ejecutable */
-	for (i = 0 ; i < bsp_next_dev ; i++)
-		if (!memcmp (bsp_dev_list[i].name, pathname, len))
-			return &bsp_dev_list[i];
+  /* Usamos memcmp() en vez de strcmp() para reducir el tamaño del ejecutable */
+  for (i = 0 ; i < bsp_next_dev ; i++)
+    if (!memcmp (bsp_dev_list[i].name, pathname, len))
+      return &bsp_dev_list[i];
 
-	return NULL;
+  return NULL;
 }
 
 /*****************************************************************************/
@@ -127,9 +121,8 @@ bsp_dev_t * find_dev (const char *pathname)
  * Retorna el puntero del dispositivo asociado al descriptor de un fichero
  * @param fd   El descriptor
  */
-inline bsp_dev_t* get_dev (uint32_t fd)
-{
-	return bsp_fd_list[fd].dev;
+inline bsp_dev_t* get_dev (uint32_t fd){
+  return bsp_fd_list[fd].dev;
 }
 
 /*****************************************************************************/
@@ -138,9 +131,8 @@ inline bsp_dev_t* get_dev (uint32_t fd)
  * Retorna los flags de apertura de un fichero
  * @param fd   El descriptor
  */
-inline int get_flags (uint32_t fd)
-{
-	return bsp_fd_list[fd].flags;
+inline int get_flags (uint32_t fd){
+  return bsp_fd_list[fd].flags;
 }
 
 /*****************************************************************************/
@@ -152,24 +144,21 @@ inline int get_flags (uint32_t fd)
  * @return 		El numero de descriptor o -1 en caso de error. La condición de error
  * 				se indica en la variable global errno.
  */
-int32_t get_fd(bsp_dev_t *dev, int flags)
-{
-	int32_t i;
+int32_t get_fd(bsp_dev_t *dev, int flags){
+  int32_t i;
 
-	for (i = 0; i < BSP_MAX_FD; i++)
-	{
-		/* Si la entrada de la tabla está vacía, le asignamos un nuevo descriptor */
-		if (bsp_fd_list[i].dev == NULL)
-		{
-			bsp_fd_list[i].dev = dev;
-			bsp_fd_list[i].flags = flags;
-			return i;
-		}
-	}
+  for (i = 0; i < BSP_MAX_FD; i++){
+    /* Si la entrada de la tabla está vacía, le asignamos un nuevo descriptor */
+    if (bsp_fd_list[i].dev == NULL){
+      bsp_fd_list[i].dev = dev;
+      bsp_fd_list[i].flags = flags;
+      return i;
+    }
+  }
 
-	/* Se ha alcanzado el máximo número de ficheros abiertos */
-	errno = ENFILE;
-	return -1;
+  /* Se ha alcanzado el máximo número de ficheros abiertos */
+  errno = ENFILE;
+  return -1;
 }
 
 /*****************************************************************************/
@@ -179,13 +168,11 @@ int32_t get_fd(bsp_dev_t *dev, int flags)
  * asignados a la E/S estándar (0, 1 y 2) no pueden ser liberados.
  * @param fd Número del descriptor
  */
-void release_fd (uint32_t fd)
-{
-	if (fd > 2)
-	{
-		bsp_fd_list[fd].dev   = NULL;
-		bsp_fd_list[fd].flags = 0;
-	}
+void release_fd (uint32_t fd){
+  if (fd > 2){
+    bsp_fd_list[fd].dev   = NULL;
+    bsp_fd_list[fd].flags = 0;
+  }
 }
 
 /*****************************************************************************/
@@ -199,19 +186,17 @@ void release_fd (uint32_t fd)
  * @param flags Configuración
  * @param mode  Modo de apertura
  */
-void redirect_fd(uint32_t fd, const char* name, int flags, mode_t mode)
-{
-	int temp;
+void redirect_fd(uint32_t fd, const char* name, int flags, mode_t mode){
+  int temp;
 
-	temp = open (name, flags, mode);
+  temp = open (name, flags, mode);
 
-	if (temp >= 0)
-	{
-		bsp_fd_list[fd].dev   = bsp_fd_list[temp].dev;
-		bsp_fd_list[fd].flags = bsp_fd_list[temp].flags;
-
-		release_fd (temp);
-	}
+  if (temp >= 0){
+    bsp_fd_list[fd].dev   = bsp_fd_list[temp].dev;
+    bsp_fd_list[fd].flags = bsp_fd_list[temp].flags;
+    
+    release_fd (temp);
+  }
 } 
 
 /*****************************************************************************/
--- bsp/hal/syscalls.c
+++ bsp/hal/syscalls.c
@@ -6,7 +6,6 @@
 #include <sys/types.h>
 #include <reent.h>
 #include <errno.h>
-
 #include "system.h"
 
 /*****************************************************************************/
@@ -26,35 +25,32 @@ extern void _heap_start, _heap_end;
  * @return		Un puntero al nuevo bloque de memoria asignado o -1 en caso de error.
  * 				La condición de error se indica en la variable global errno.
  */
-void * _sbrk (intptr_t incr)
-{
-	static void *current_break = &_heap_start;
-	void *last_break = current_break;
+void * _sbrk (intptr_t incr){
+  static void *current_break = &_heap_start;
+  void *last_break = current_break;
 
-	/* Anulamos las interrupciones durante el proceso de reserva */
-	/* Comienzo de la sección crítica */
-	itc_disable_ints();
+  /* Anulamos las interrupciones durante el proceso de reserva */
+  /* Comienzo de la sección crítica */
+  itc_disable_ints();
 
-	/* Forzamos a que el incremento sea un múltiplo del tamaño de la palabra */
-	incr = (intptr_t) (((unsigned int)incr + 3) & ~3);
+  /* Forzamos a que el incremento sea un múltiplo del tamaño de la palabra */
+  incr = (intptr_t) (((unsigned int)incr + 3) & ~3);
 
-	/* Comprobamos que hay memoria suficiente en el heap */
-	if (current_break + incr > &_heap_end)
-	{
-		errno = ENOMEM;
-		last_break = (void *) -1;
-	}
-	else
-	{
-		/* Ampliamos el área reservada para datos dinámicos */
-		current_break += incr;
-	}
+  /* Comprobamos que hay memoria suficiente en el heap */
+  if (current_break + incr > &_heap_end){
+    errno = ENOMEM;
+    last_break = (void *) -1;
+  }
+  else{
+    /* Ampliamos el área reservada para datos dinámicos */
+    current_break += incr;
+  }
 
-	/* Volvemos a habilitar las interrupciones */
-	/* Fin de la sección crítica */
-	itc_restore_ints();
+  /* Volvemos a habilitar las interrupciones */
+  /* Fin de la sección crítica */
+  itc_restore_ints();
 
-	return last_break;
+  return last_break;
 }
 
 /*****************************************************************************/
@@ -67,11 +63,19 @@ void * _sbrk (intptr_t incr)
  * @return			El descriptor del dispositivo o -1 en caso de error.
  * 					La condición de error se indica en la variable global errno.
  */
-int _open(const char *pathname, int flags, mode_t mode)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 10 */
+int _open(const char *pathname, int flags, mode_t mode){
+  bsp_dev_t * dev = find_dev(pathname);
 
-	return -1;
+  // Si exsite el dispositivo
+  if(dev){
+    if(dev -> open == NULL || dev -> open(dev -> id, flags, mode) >= 0)
+      return get_fd(dev, flags);
+  }
+  else
+    errno = ENODEV;
+
+  // Se devuelve -1 si la función open falla o no hay función open, o si el dispositivo no existe
+  return -1;
 }
 
 /*****************************************************************************/
@@ -82,10 +86,17 @@ int _open(const char *pathname, int flags, mode_t mode)
  * @return		0 en caso de éxito o -1 en caso de error.
  * 				La condición de error se indica en la variable global errno.
  */
-int _close (int fd)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 10 */
-	return -1;
+int _close (int fd){
+  bsp_dev_t *dev = get_dev(fd);
+  release_fd(fd);
+
+  if(dev && dev -> close)
+    return dev -> close(dev -> id);
+  else
+    errno = EBADF;
+
+  // Se devuelve -1 si la función close falla o no hay función close, o si el dispositivo no existe
+  return -1;
 }
 
 /*****************************************************************************/
@@ -98,10 +109,16 @@ int _close (int fd)
  * @return		El número de bytes leidos o -1 en caso de error.
  * 				La condición de error se indica en la variable global errno.
  */
-ssize_t _read(int fd, char *buf, size_t count)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 10 */
-	return 0;
+ssize_t _read(int fd, char *buf, size_t count){
+  bsp_dev_t *dev = get_dev(fd);
+
+  // Devuelve el número de bytes leídos si la función read no falla
+  //  Si falla devuelve -1
+  // Si no existe devuelve 0
+  if(dev && dev -> read)
+    return dev -> read(dev -> id, buf, count);
+  else
+    return 0;
 }
 
 /*****************************************************************************/
@@ -111,13 +128,19 @@ ssize_t _read(int fd, char *buf, size_t count)
  * @param fd	Descriptor de fichero/dispositivo
  * @param buf	Puntero al búfer que almacena los datos
  * @param count	Número de bytes que se quieren escribir
- * @return		El número de bytes escritos o -1 en caso de error.
- * 				La condición de error se indica en la variable global errno.
+ * @return	El número de bytes escritos o -1 en caso de error.
+ * 		La condición de error se indica en la variable global errno.
  */
-ssize_t _write (int fd, char *buf, size_t count)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 10 */
-	return count;
+ssize_t _write (int fd, char *buf, size_t count){
+  bsp_dev_t *dev = get_dev(fd);
+
+  // Devuelve el número de bytes escritos si la función read no falla
+  //  Si falla devuelve -1
+  // Si no existe devuelve el número de bytes a escribir
+  if(dev && dev -> write)
+    return dev -> write(dev -> id, buf, count);
+  else
+    return count;
 }
 
 /*****************************************************************************/
@@ -127,13 +150,20 @@ ssize_t _write (int fd, char *buf, size_t count)
  * @param fd		Descriptor de fichero/dispositivo
  * @param offset	Desplazamiento
  * @param whence	Base para el desplazamiento
- * @return			El nuevo desplazamiento medido en bytes desde el inicio del fichero o -1 en caso de error.
- * 					La condición de error se indica en la variable global errno
+ * @return              El nuevo desplazamiento medido en bytes 
+ *                      desde el inicio del fichero o -1 en caso de error.
+ * 			La condición de error se indica en la variable global errno
  */
-off_t _lseek(int fd, off_t offset, int whence)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 10 */
-	return 0;
+off_t _lseek(int fd, off_t offset, int whence){
+  bsp_dev_t *dev = get_dev(fd);
+
+  // Devuelve 0 si no hay función lseek o no existe
+  // -1 si falla
+  // El buyes desplazados si todo ha funcionado correctamente
+  if(dev && dev -> lseek)
+    return dev -> lseek(dev -> id, offset, whence);
+  else
+    return 0;
 }
 
 /*****************************************************************************/
@@ -142,13 +172,19 @@ off_t _lseek(int fd, off_t offset, int whence)
  * Obtención de información de un dispositivo/fichero
  * @param fd	Descriptor de fichero/dispositivo
  * @param buf	Estructura para almacenar dicha información
- * @return		0 o -1 en caso de error.
- * 				La condición de error se indica en la variable global errno
+ * @return	0 o -1 en caso de error.
+ * 		La condición de error se indica en la variable global errno
  */
-int _fstat(int fd, struct stat *buf)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 10 */
-	return 0;
+int _fstat(int fd, struct stat *buf){
+  bsp_dev_t *dev = get_dev(fd);
+
+  if(dev && dev -> fstat)
+    return dev -> fstat(dev -> id, buf);
+  else{
+    // Modo = dispositivo caracteres
+    buf -> st_mode = S_IFCHR;
+    return 0;
+  }
 }
 
 /*****************************************************************************/
@@ -159,10 +195,15 @@ int _fstat(int fd, struct stat *buf)
  * @return		1 en caso de que el descriptor corresponda a una terminal o 0 en otro caso.
  * 				La condición de error se indica en la variable global errno
  */
-int _isatty (int fd)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 10 */
-	return 1;
+int _isatty (int fd){
+  bsp_dev_t * dev = get_dev(fd);
+
+  if(dev && dev -> isatty)
+    return dev -> isatty(dev -> id);
+  else{
+    // Es un dispositivo de caracteres o no tiene función isatty, o  dev no existe
+    return 1;
+  }
 }
 
 /*****************************************************************************/
